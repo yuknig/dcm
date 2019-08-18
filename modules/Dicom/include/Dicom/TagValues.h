d35adc1237bbac8cbc132a0f42efc87ae2c60faf
yuknig <iknigavko@gmail.com>
2019-08-18 23:12:58 +0300

    Introduce new data structures for handling Tag, GroupTags.

--- modules/Dicom/include/Dicom/TagValues.h
+++ modules/Dicom/include/Dicom/TagValues.h
@@ -7,6 +7,7 @@
 #include <Dicom/Util.h>
 #include <Util/Stream.h>
 #include <Util/MVector.h>
+#include "Util/optional.h"
 
 #include <vector>
 #include <bitset>
@@ -191,6 +192,162 @@ public://data
 };
 
 typedef std::shared_ptr<Group> GroupPtr;
+
+
+
+class TagValue {
+public:
+    // no value ctor
+    explicit TagValue(const Tag& a_tag, const VRType& a_vr)
+        : m_tag(a_tag)
+        , m_value(0u)
+        , m_storage(ValueStorage::NoValue)
+        , m_vr(a_vr)
+    {}
+
+    // single small value ctor (<= 4bytes)
+    explicit TagValue(const Tag& a_tag, const VRType& a_vr, const SingleValueUnion& a_value)
+        : m_tag(a_tag)
+        , m_value(a_value)
+        , m_storage(ValueStorage::Inplace)
+        , m_vr(a_vr)
+    {}
+
+    // big or multiple value ctor( > 4 bytes)
+    explicit TagValue(const Tag& a_tag, const VRType& a_vr, const uint32_t a_offset_in_buf)
+        : m_tag(a_tag)
+        , m_value(a_offset_in_buf)
+        , m_storage(ValueStorage::Buffer)
+        , m_vr(a_vr)
+    {}
+
+    const Tag& tag() const {
+        return m_tag;
+    }
+
+private:
+    enum class ValueStorage: uint16_t {
+        NoValue = 0,
+        Inplace,
+        Buffer
+    };
+
+private:
+    Tag m_tag;
+    SingleValueUnion m_value;
+    ValueStorage m_storage;
+    VRType m_vr;
+};
+
+class TagVector {
+public:
+    template <typename... ArgsT>
+    void emplace(ArgsT&&... a_args) {
+        m_tags.emplace_back(std::forward<ArgsT>(a_args)...);
+        if (m_sorted && m_tags.size() > 1) // check that vector is still sorted
+            m_sorted = m_tags[m_tags.size() - 2].tag() < m_tags.back().tag()
+    }
+
+    bool HasTag(const Tag& a_tag) const {
+        return (nullptr != GetTagPtr(a_tag));
+    }
+
+    void Sort() {
+        std::sort(m_tags.begin(), m_tags.end(),
+            [](const TagValue& lhs, const TagValue& rhs) {
+            return lhs.tag() < rhs.tag();
+        });
+    }
+
+private:
+    const TagValue* GetTagPtr(const Tag& a_tag) const {
+        return m_sorted ? GetTagPtr_Sorted(a_tag)
+                        : GetTagPtr_Unsorted(a_tag);
+    }
+
+    const TagValue* GetTagPtr_Sorted(const Tag& a_tag) const {
+        assert(m_sorted);
+        auto find_it = std::lower_bound(m_tags.begin(), m_tags.end(), a_tag,
+            [](const TagValue& lhs, const Tag& rhs) {
+                return lhs.tag() < rhs;
+        });
+        if (m_tags.end() == find_it || a_tag != find_it->tag())
+            return nullptr;
+        return &*find_it;
+    }
+
+    const TagValue* GetTagPtr_Unsorted(const Tag& a_tag) const {
+        auto find_it = std::find_if(m_tags.begin(), m_tags.end(),
+            [a_tag](const TagValue& tag_value) {
+            return tag_value.tag() == a_tag;
+        });
+        if (m_tags.end() == find_it)
+            return nullptr;
+        return &*find_it;
+    }
+
+private:
+    std::vector<TagValue> m_tags;
+    bool m_sorted = true;
+};
+
+class NewGroup {
+public:
+    template <typename T>
+    void AddTag(const Tag a_tag, const VRType a_vr, const uint32_t a_valueElements, StreamRead& a_stream) {
+
+        if (1 < a_valueElements) { // multiple values
+            const auto offset = LoadDataToBuf(a_stream, a_valueElements * sizeof(T));
+            if (offset.has_value())
+                m_tags.emplace(a_tag, a_vr, *offset);
+        }
+        else if (1 == a_valueElements) // single value
+            m_tags.emplace(a_tag, a_vr, SingleValueUnion(a_stream.read<T>()));
+        else
+            m_tags.emplace(a_tag, a_vr); // no value
+    }
+
+private:
+    std::optional<uint32_t> LoadDataToBuf(StreamRead& a_stream, uint32_t bytes) {
+        const size_t elms_old = m_data_buf.size();
+        const size_t elms_add = (bytes + 3) / 4 + 1; // aligned uint32_t and 1 extra element to hold length
+        const size_t elms_new = elms_old + elms_add;
+
+        // try to pre-reserve some size to reduce number of reallocations
+        if (elms_new > m_data_buf.capacity())
+        {
+            size_t elms_to_reserve = elms_old + elms_old / 2; // 1.5x increase
+            elms_to_reserve = ((elms_to_reserve + 15) / 16) * 16 ; // align to 16 dwords
+            if (elms_to_reserve > elms_new &&
+                elms_to_reserve - elms_new <= 1024)
+                m_data_buf.reserve(elms_to_reserve);
+        }
+
+        m_data_buf.resize(elms_new, 0u);
+        m_data_buf[elms_old] = bytes;
+        const size_t bytes_readed = a_stream.readToMem(m_data_buf.data() + elms_old + 1, bytes);
+        if (bytes_readed != bytes) {
+            m_data_buf.resize(elms_old); // resize back
+            return std::nullopt;
+        }
+        return static_cast<uint32_t>(elms_old);
+    }
+
+    bool HasTag(const Tag a_tag) const {
+        return m_tags.HasTag(a_tag);
+    }
+
+    template <typename T>
+    GetValueResult getTag(const Tag a_tag, T& a_value) const {
+        const auto tag_ptr = m_tags.GetTagPtr(a_tag);
+        if (!tag_ptr)
+            return GetValueResult::DoesNotExists;
+    }
+
+private:
+    TagVector m_tags;
+    std::vector<uint32_t> m_data_buf;
+};
 
 class Sequence: public Tag_ValuePtr<std::vector<std::shared_ptr<Group>>>
 {
