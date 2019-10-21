d89787321b0e7b6ebe7d62f670cce4ff6d81786a
yuknig <iknigavko@gmail.com>
2019-10-21 22:20:01 +0300

    MVector: change function scope style.

--- modules/Dicom/include/Util/MVector.h
+++ modules/Dicom/include/Util/MVector.h
@@ -4,94 +4,78 @@
 #include <new>
 #include <memory>
 
-struct FreeDeleter
-{
-    void operator()(void* x)
-    {
+struct FreeDeleter {
+    void operator()(void* x) {
         free(x);
     }
 };
 
 template <typename T, typename SizeT = size_t, bool Reallocatable = false>
-class MVector
-{
+class MVector {
     static_assert(std::is_unsigned<SizeT>::value, "Size type should be unsigned");
 
 public:
-    MVector()
-    {}
+    MVector() {
+    }
 
-    ~MVector()
-    {
+    ~MVector() {
         deleteObjects();
         m_data.reset();
         m_size_alloc = 0;
     }
 
     template <typename... ArgsT>
-    void emplace_back(ArgsT&&... a_args)
-    {
+    void emplace_back(ArgsT&&... a_args) {
         checkAllocSize();
 
         new (m_data.get() + m_size_used) T {std::forward<ArgsT>(a_args)...};
         ++m_size_used;
     }
 
-    void resize(size_t a_new_size)
-    {
+    void resize(size_t a_new_size) {
         reallocate(a_new_size);
     }
 
-    SizeT size() const
-    {
+    SizeT size() const {
         return m_size_used;
     }
 
-    bool empty() const
-    {
+    bool empty() const {
         return (0 == size());
     }
 
-    const T& operator[](const size_t a_pos) const
-    {
+    const T& operator[](const size_t a_pos) const {
         assert(a_pos < m_size_used);
 
         return *(m_data.get() + a_pos);
     }
 
-    const T* data() const
-    {
+    const T* data() const {
         return m_data.get();
     }
 
-    T* data()
-    {
+    T* data() {
         return m_data.get();
     }
 
-    const T* cbegin() const
-    {
+    const T* cbegin() const {
         return m_data.get();
     }
 
-    T* begin()
-    {
+    T* begin() {
         return m_data.get();
     }
 
-    const T* cend() const
-    {
+    const T* cend() const {
         return cbegin() + m_size_used;
     }
 
-    T* end()
-    {
+    T* end() {
         return begin() + m_size_used;
     }
 
 private: // functions
-    void checkAllocSize()
-    {
+    void checkAllocSize() {
         if (m_size_alloc > m_size_used)
             return;
 
@@ -99,8 +83,7 @@ private: // functions
         reallocate(new_size_alloc);
     }
 
-    static SizeT sizeIncrement(size_t a_size)
-    {
+    static SizeT sizeIncrement(size_t a_size) {
         const size_t result = 1 < a_size ? a_size + a_size / 2 : 4;
         if (std::numeric_limits<SizeT>::max() < result)
             throw std::runtime_error("Too many elements");
@@ -109,8 +92,7 @@ private: // functions
 
     template <bool Enabled = Reallocatable,
               typename std::enable_if<Enabled, int>::type = 0>
-    void reallocate(const SizeT a_new_size)
-    {
+    void reallocate(const SizeT a_new_size) {
         auto* old_data = m_data.release();
         m_data = std::unique_ptr<T, FreeDeleter>(static_cast<T*>(realloc(old_data, a_new_size * sizeof(T))));
         if (!m_data)
@@ -123,8 +105,7 @@ private: // functions
               bool Enabled = Reallocatable,
               typename std::enable_if<std::is_move_constructible<U>::value &&
                                       !Reallocatable, int>::type = 0>
-    void reallocate(const SizeT a_new_size)
-    {
+    void reallocate(const SizeT a_new_size) {
         std::unique_ptr<T, FreeDeleter> new_data(static_cast<T*>(malloc(a_new_size * sizeof(T))));
         if (!new_data)
             throw std::runtime_error("Failed to alloc SortedTagList");
@@ -138,8 +119,7 @@ private: // functions
 
     template <typename U = T,
               typename std::enable_if<std::is_destructible<U>::value, int>::type = 0>
-    void deleteObjects()
-    {
+    void deleteObjects() {
         for (size_t i = 0; i < m_size_used; ++i)
             (m_data.get() + i)->~T();
         m_size_used = 0;
@@ -147,14 +127,11 @@ private: // functions
 
     template <typename U = T,
               typename std::enable_if<!std::is_destructible<U>::value, int>::type = 0>
-    void deleteObjects()
-    {
+    void deleteObjects() {
         m_size_used = 0;
     }
 
-
-
-private: // data
+private:
     std::unique_ptr<T, FreeDeleter> m_data;
     SizeT m_size_alloc = 0;
     SizeT m_size_used = 0;
