473cb6dd9637aff8fab1a7601f4b18032ca15e87
yuknig <iknigavko@gmail.com>
2019-10-21 22:24:19 +0300

    MVector.h: rename variables:
    m_size_alloc -> m_capacity
    m_size_used -> m_size

--- modules/Dicom/include/Util/MVector.h
+++ modules/Dicom/include/Util/MVector.h
@@ -23,15 +23,15 @@ public:
     ~MVector() {
         deleteObjects();
         m_data.reset();
-        m_size_alloc = 0;
+        m_capacity = 0;
     }
 
     template <typename... ArgsT>
     void emplace_back(ArgsT&&... a_args) {
         checkAllocSize();
 
-        new (m_data.get() + m_size_used) T {std::forward<ArgsT>(a_args)...};
-        ++m_size_used;
+        new (m_data.get() + m_size) T {std::forward<ArgsT>(a_args)...};
+        ++m_size;
     }
 
     void resize(size_t a_new_size) {
@@ -39,7 +39,7 @@ public:
     }
 
     SizeT size() const {
-        return m_size_used;
+        return m_size;
     }
 
     bool empty() const {
@@ -47,7 +47,7 @@ public:
     }
 
     const T& operator[](const size_t a_pos) const {
-        assert(a_pos < m_size_used);
+        assert(a_pos < m_size);
 
         return *(m_data.get() + a_pos);
     }
@@ -69,19 +69,19 @@ public:
     }
 
     const T* cend() const {
-        return cbegin() + m_size_used;
+        return cbegin() + m_size;
     }
 
     T* end() {
-        return begin() + m_size_used;
+        return begin() + m_size;
     }
 
 private: // functions
     void checkAllocSize() {
-        if (m_size_alloc > m_size_used)
+        if (m_capacity > m_size)
             return;
 
-        const auto new_size_alloc = sizeIncrement(m_size_alloc);
+        const auto new_size_alloc = sizeIncrement(m_capacity);
         reallocate(new_size_alloc);
     }
 
@@ -100,7 +100,7 @@ private: // functions
         if (!m_data)
             throw std::runtime_error("Failed realloc SortedTagList");
 
-        m_size_alloc = a_new_size;
+        m_capacity = a_new_size;
     }
 
     template <typename U = T,
@@ -112,31 +112,31 @@ private: // functions
         if (!new_data)
             throw std::runtime_error("Failed to alloc SortedTagList");
 
-        for(size_t i = 0; i < m_size_used; ++i)
+        for(size_t i = 0; i < m_size; ++i)
             new (new_data.get() + i) T (std::move(*(m_data.get() + i)));
 
         m_data.swap(new_data);
-        m_size_alloc = a_new_size;
+        m_capacity = a_new_size;
     }
 
     template <typename U = T,
               typename std::enable_if<std::is_destructible<U>::value, int>::type = 0>
     void deleteObjects() {
-        for (size_t i = 0; i < m_size_used; ++i)
+        for (size_t i = 0; i < m_size; ++i)
             (m_data.get() + i)->~T();
-        m_size_used = 0;
+        m_size = 0;
     }
 
     template <typename U = T,
               typename std::enable_if<!std::is_destructible<U>::value, int>::type = 0>
     void deleteObjects() {
-        m_size_used = 0;
+        m_size = 0;
     }
 
 private:
     std::unique_ptr<T, FreeDeleter> m_data;
-    SizeT m_size_alloc = 0;
-    SizeT m_size_used = 0;
+    SizeT m_capacity = 0;
+    SizeT m_size = 0;
 };
 
 #endif // _MVECTOR_095A6230_53C4_44F3_9960_EA0D824802FF_
