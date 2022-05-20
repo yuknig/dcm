f1efd65da78435520f67d4d665f791847a9bf047
yuknig <i.knigavko@gmail.com>
2022-05-21 01:36:22 +0300

    Check if cmd command succeeded.
    Change naming of variable holding path to build directory.

--- init_project.bat
+++ init_project.bat
@@ -2,8 +2,9 @@ git submodule update --init --recursive
 
 call set_path.bat
 
-mkdir project\%BUILD_DIR%
-cd project\%BUILD_DIR%
+set BUILD_DIR=project\%BUILD_SUBDIR%
+if not exist %BUILD_DIR% mkdir %BUILD_DIR% || exit /B 1
+cd %BUILD_DIR% || exit /B 1
 
 set _CMAKE_ARGS_=-DBOOST_DIR=%BOOST_DIR%; -Dgtest_force_shared_crt=ON
-cmake.exe ..\.. -G %_CMAKE_GEN_% %_CMAKE_ARGS_%
+cmake.exe ..\.. -G %_CMAKE_GEN_% %_CMAKE_ARGS_% || exit /B 1
