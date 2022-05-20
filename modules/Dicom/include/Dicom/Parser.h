fbe7a973c68f08adf5b8d462cc5d48c4b7f389a4
yuknig <i.knigavko@gmail.com>
2022-05-20 23:18:23 +0300

    Move TagDesc class to dedicated file.

--- modules/Dicom/include/Dicom/Parser.h
+++ modules/Dicom/include/Dicom/Parser.h
@@ -11,31 +11,6 @@
 namespace dcm
 {
 
-struct TagDesc
-{
-    TagDesc() noexcept
-        : TagDesc(0u, 0u, 0u, 0u, VRType::Undefined, false)
-    {}
-
-    TagDesc(Tag a_tag, uint32_t a_fullLength, uint32_t a_valueLength, uint8_t a_valueOffset, VRType a_vr, bool a_nested)
-        : m_tag(a_tag)
-        , m_fullLength(a_fullLength)
-        , m_valueLength(a_valueLength)
-        , m_valueOffset(a_valueOffset)
-        , m_nested(a_nested)
-        , m_vr(a_vr)
-    {}
-
-public://data
-    Tag      m_tag;
-    uint32_t m_fullLength;
-    uint32_t m_valueLength;
-    uint8_t  m_valueOffset;
-    bool     m_nested;
-    VRType   m_vr;
-    //TODO: add flag is length defined
-};
-
 class Parser
 {
 public://functions
