eeb09c00312ac97ef9069fd32257f67c07055444
yuknig <i.knigavko@gmail.com>
2022-05-21 01:26:09 +0300

    Remove bat devoted to build with other versions of VisualStudio as obsolete.

--- build_vs_2015_32.bat
+++
@@ -1,5 +0,0 @@
-set BUILD_DIR="msvc2015_x32"
-set _CMAKE_GEN_="Visual Studio 14"
-call init_project.bat
-
-start Dcm.sln
--- build_vs_2017_clang.bat
+++
@@ -1,6 +0,0 @@
-set BUILD_DIR="msvc2017_x32_clang"
-set _CMAKE_GEN_="Visual Studio 15"
-set _CMAKE_GEN_ARCH="LLVM-vs2014"
-call init_project.bat
-
-start Dcm.sln
